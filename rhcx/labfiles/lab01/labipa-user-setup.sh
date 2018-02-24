9c36a50ff47b7a03df5d39a124c11b35eb43b80f
dmbrownlee <dmbrownlee@users.noreply.github.com>
2018-02-23 21:19:35 -0800

    Add a script for configuring the lab users.

---
+++ rhcx/labfiles/lab01/labipa-user-setup.sh
@@ -0,0 +1,6 @@
+PASSWORD=password
+echo $PASSWORD | kinit admin
+for u in ldapuser1 ldapuser2 ldapuser3 ldapuser4 ldapuser5 isabelle; do
+  echo | ipa user-add --first $u --last $u $u
+  echo -e "$PASSWORD\n$PASSWORD" | ipa passwd $u
+done
