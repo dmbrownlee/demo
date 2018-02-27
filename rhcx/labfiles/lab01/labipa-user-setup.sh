fd845cb2a0f66430349cefe0cbc0443b696867f8
dmbrownlee <dmbrownlee@users.noreply.github.com>
2018-02-26 22:13:20 -0800

    Move IPA user setup from shell script to Ansible.

--- rhcx/labfiles/lab01/labipa-user-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PASSWORD=password
-echo $PASSWORD | kinit admin
-for u in ldapuser1 ldapuser2 ldapuser3 ldapuser4 ldapuser5 isabelle; do
-  echo | ipa user-add --first $u --last $u $u
-  echo -e "$PASSWORD\n$PASSWORD" | ipa passwd $u
-done
