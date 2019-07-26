e12c876c0139ac1f28e2a23ab713bd67f43b87f7
Andrew Gertz <agertz@google.com>
2019-07-26 11:07:12 -0700

    emoving unnecessary file (foo.sh) :

--- foo.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-
-curl https://releases.hashicorp.com/vagrant/2.2.4/vagrant/vagrant_2.2.4_x86_64.deb > vagrant_2.2.4_x86_64.deb
-sudo dpkg -i vagrant_2.2.4_x86_64.deb
-vagrant plugin install vagrant-google
-
-
