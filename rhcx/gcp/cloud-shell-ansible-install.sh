7785525a91ecc8ba77e28eaf279e397dd0857e9e
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2020-02-15 08:51:48 -0800

    Add script to install ansible in cloud shell.

---
+++ rhcx/gcp/cloud-shell-ansible-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+[ "$CLOUD_SHELL" == 'true' ] || {
+  echo 'This script is intended for use only within a GCP Cloud Shell.'
+  exit 1
+}
+
+which ansible 2>&1 > /dev/null
+if [ $? -ne 0 ]; then
+  pip3 install --user ansible
+  [[ "$PATH" =~ "$HOME/.local/bin" ]] || {
+    echo '[[ "$PATH" =~ "$HOME/.local/bin" ]] || export PATH=$PATH:$HOME/.local/bin' >> $HOME/.bashrc
+    export PATH=$PATH:$HOME/.local/bin
+  }
+fi
+
+ansible-playbook gcp-tf-setup.yml
