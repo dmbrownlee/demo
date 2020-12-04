fbb4d7b4f2227c488c3d4ee17afbde861e2a8195
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2020-12-04 07:26:11 -0800

    Activate all hostonly interfaces, not just the last (in case the user installs others).

--- ansible/roles/gns3_host/files/vboxnet0.zsh
+++ ansible/roles/gns3_host/files/vboxnet0.zsh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-$(/usr/local/bin/vboxmanage list hostonlyifs | awk '/^Name:/{ NAME=$2}; /^IPAddress:/{ IP=$2 }; END { print "/usr/local/bin/vboxmanage hostonlyif ipconfig", NAME, "--ip", IP; }')
+/usr/local/bin/vboxmanage list hostonlyifs | awk '/^Name:/{ NAME=$2}; /^IPAddress:/{ IP=$2 }; /^Status:/ { print "/usr/local/bin/vboxmanage hostonlyif ipconfig", NAME, "--ip", IP; }' | zsh 
