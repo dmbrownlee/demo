b63b46b0b9ee81e93264dc6a6c4120c56bad634b
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2021-12-10 18:50:02 -0800

    Update files to build alpine image for GNS3 containers.

--- host_install/Docker/alpine/files/boot.sh
+++ host_install/Docker/alpine/files/boot.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 if [[ $ANSIBLE_CONTROLLER == 'true' ]]; then
   which curl || apk add --no-cache curl
-  [ -f /home/ansible/setup ] ||
-    curl --output /home/ansible/setup https://raw.githubusercontent.com/dmbrownlee/labnetwork/main/setup
-  chmod +x /home/ansible/setup
+  [ -f /home/USERNAME/setup ] ||
+    curl --output /home/USERNAME/setup https://raw.githubusercontent.com/dmbrownlee/labnetwork/main/setup
+  chmod +x /home/USERNAME/setup
 fi
 
-/usr/sbin/sshd
+[ -f /etc/ssh/ssh_host_rsa_key ] ||
+  ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
+
+/sbin/runsvdir -P /etc/service &
 
 while true; do
+  # vtysh is Quagga's IOS-like shell which we would prefer on routers
   ( [ -f /root/startvtysh ] && /usr/bin/vtysh ) ||
-  ( grep -q ^student /etc/passwd && su - student ) ||
-  ( grep -q ^ansible /etc/passwd && su - ansible ) ||
+  ( grep -q ^USERNAME: /etc/passwd && su - USERNAME ) ||
   /bin/bash
 done
